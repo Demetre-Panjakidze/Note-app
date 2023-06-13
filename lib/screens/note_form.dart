a89f47ebbd12e8dce82abc303043bdbb4fbd3ab0
Demetre-Panjakidze <demetrepanjakidze1@gmail.com>
2023-06-13 19:13:13 +0400

    bla

--- lib/screens/note_form.dart
+++ lib/screens/note_form.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:test/data/note_data.dart';
+import 'package:test/models/note_model.dart';
 
 class NoteFormScreen extends StatefulWidget {
   const NoteFormScreen({super.key});
@@ -11,17 +13,75 @@ class _NoteFormScreenState extends State<NoteFormScreen> {
   final _formKey = GlobalKey<FormState>();
   String _enteredText = '';
   String _enteredTitle = '';
-  DateTime _date = DateTime.now();
+
+  void saveForm() {
+    _formKey.currentState!.save();
+    setState(() {
+      notes.add(
+        NoteItem(
+          text: _enteredText,
+          title: _enteredTitle,
+          date: DateTime.now(),
+        ),
+      );
+    });
+
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Form')),
+      appBar: AppBar(
+        title: const Text('Form'),
+        actions: [
+          IconButton(
+            onPressed: saveForm,
+            icon: const Icon(Icons.save),
+          ),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Form(
+          key: _formKey,
           child: Column(
-            children: [],
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Title..',
+                  hintStyle: TextStyle(color: Colors.grey),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                ),
+                maxLength: 50,
+                onSaved: (value) {
+                  _enteredTitle = value!;
+                },
+              ),
+              const SizedBox(
+                width: 32,
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextFormField(
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Enter your note',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                    ),
+                    onChanged: (value) {
+                      _enteredText = value;
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
